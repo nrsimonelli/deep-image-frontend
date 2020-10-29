c5ffabd2b219e32e2a5369b7335047423e6eeae7
Nicholas Simonelli <nrsimonelli@gmail.com>
2020-10-28 19:06:14 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from "react";
+import ImageUploader from "react-images-upload";
 
-function App() {
+const App = props => {
+  const [pictures, setPictures] = useState([]);
+
+  const onDrop = picture => {
+    setPictures([...pictures, picture]);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+    <div>
+      Deep Image
+    </div>
+    <div className='upload-block'>
+      <div>Upload Image</div>
+      <div>Choose Style</div>
+    </div>
+    <ImageUploader
+      {...props}
+      withIcon={true}
+      onChange={onDrop}
+      imgExtension={[".jpg", ".gif", ".png", ".gif"]}
+      maxFileSize={5242880}
+    />
     </div>
   );
-}
+};
 
 export default App;
